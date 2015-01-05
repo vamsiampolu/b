8e5700be6606727b3b6929306b6896df050c8e76
vamsiampolu <vamsideepak03@gmail.com>
2015-01-05 15:05:24 +0530

    Added all files,initial test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var $=require('jquery');
 var semanticUi=require('semantic-ui')($);
-
+console.log(semanticUi);
 /*now to test the working of bootstrap,build a navbar with css and a menu item that opens a modal*/
 
