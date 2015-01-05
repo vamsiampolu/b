253d1d575d39870f12bd9702ce9903f4f0c19aad
vamsiampolu <vamsideepak03@gmail.com>
2015-01-05 15:45:00 +0530

    Last commit for semantic-ui branch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $("#myButton").on("click",function(){
 	console.log("sidebar button clicked");
 	console.log($.fn.siebar);
 	$('#mySidebar').sidebar('toggle');
-})
+});
 
 /*now to test the working of bootstrap,build a navbar with css and a menu item that opens a modal*/
 
