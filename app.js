196c08f7ed9213b740278793613fc33735e948a2
vamsiampolu <vamsideepak03@gmail.com>
2015-01-05 15:42:07 +0530

    Semantic-UI works with browserify now

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var $=require('jquery');
 var semanticUi=require('semantic-ui');
-console.log(semanticUi);
+$("#myButton").on("click",function(){
+	console.log("sidebar button clicked");
+	console.log($.fn.siebar);
+	$('#mySidebar').sidebar('toggle');
+})
+
 /*now to test the working of bootstrap,build a navbar with css and a menu item that opens a modal*/
 
